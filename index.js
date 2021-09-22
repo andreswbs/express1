5c4a891ac019c6ca50ecab7e159b071caa0184e3
Andres Järviste <andres.jarviste@wbscodingschool.com>
2021-09-22 22:29:35 +0300

    crud example

--- index.js
+++ index.js
@@ -9,10 +9,19 @@ function greeting(req, res) {
     return res.json(object)
 }
 
+function parameters(req, res) {
+    res.json({
+        kitchen: req.params.kitchen,
+        dish: req.params.dish
+    })
+}
+
 app.get('/', (req, res) => {
     return res.send('Hello world!')
 })
 
 app.get('/greeting', greeting)
+app.get('/cookbook/:kitchen/:dish', parameters)
+
 
 app.listen(port, ()=> console.log(`Server listening on port ${port}`))
